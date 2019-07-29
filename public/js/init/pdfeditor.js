4ecf3946873dfa45f00a3357f98a0d7015e48649
krushndayshmookh <krushndayshmookh@gmail.com>
2019-07-29 10:56:48 +0530

    init vue

---
+++ public/js/init/pdfeditor.js
@@ -0,0 +1,14 @@
+let documentsVue
+
+document.addEventListener('DOMContentLoaded', function() {
+	documentsVue = new Vue({
+		el: '#app',
+		data: {},
+
+		methods: {},
+
+		mounted: function() {}
+	})
+})
+
+
