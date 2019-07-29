1fa03bca56e7aca4c0b7d154fb507631f93f27f9
krushndayshmookh <krushndayshmookh@gmail.com>
2019-07-29 10:17:15 +0530

    base config

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+
+require('dotenv').config()
+
+const PORT = process.env.PORT
+// const DBPORT = process.env.MONGODB_URI
+
+var app = express()
+
+// mongoose.connect(DBPORT, { useNewUrlParser: true })
+// mongoose.Promise = global.Promise
+// var db = mongoose.connection
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+if (process.env.DEBUG) app.use(logger('dev'))
+
+app.use(cors())
+
+app.use(bodyParser.json({ limit: '50mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+
+app.use(express.static('public'))
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+app.use('/', require('./routes'))
+
+app.listen(PORT, err => {
+	if (err) throw err
+	console.info('Listening on port ' + PORT + '...')
+})
